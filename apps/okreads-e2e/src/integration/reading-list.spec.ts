84ece471023bd62c5c262b4099a39424d3b44985
Aleksandr <aleksandr.golovinov@nortal.com>
2022-07-18 10:51:30 +0300

    Task 3: Undo add and remove from reading list

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -15,14 +15,13 @@ describe('When: I use the reading list feature', () => {
   it('Then: I should see my reading list and notification popup', () => {
     cy.get('input[type="search"]').type('java');
     cy.get('form').submit();
+    cy.get('[data-testing="add-book-to-reading-list"]').first().click({force: true});
+    cy.get('[data-testing="toggle-reading-list"]').click();
 
-    cy.get('[data-testing="add-book-to-reading-list"]').first().click().then(() => {
-      cy.get('[data-testing="toggle-reading-list"]').click();
-      cy.get('[data-testing="remove-reading-list-item"]').click()
-      cy.get('simple-snack-bar').should(
-        'contain.text',
-        'Book removed'
-      );
-    });
+    cy.get('[data-testing="remove-reading-list-item"]').click()
+    cy.get('simple-snack-bar').should(
+      'contain.text',
+      'Book removed'
+    );
   });
 });
